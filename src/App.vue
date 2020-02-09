65ad680506f070bc8288890ac5b64ecda0b1565c
ruvim <schokun2000@gmail.com>
2020-02-09 15:37:57 +0200

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> i am created Vue.js</h1>
+    <HelloWorld></HelloWorld>
+    <Profile :user="user" @save-user="user = $event"></Profile>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Profile from './components/user/profile.vue'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, Profile
+  },
+  data(){
+    return {
+         user: {
+
+          name: 'John',
+          surname: 'Doe'
+
+        }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
